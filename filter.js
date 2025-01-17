86eb3a036c53560a8cc21174a47138654ba41544
far-code <157044309+Far-Coodinator@users.noreply.github.com>
2025-01-17 19:11:44 +0300

    Add files via upload

--- filter.js
+++ filter.js
@@ -1,15 +1,75 @@
-import dataBase from "./DataBase.js";document.querySelector('#filter .apply .apply-filter').addEventListener('click',()=>{const categoriesElement=document.querySelectorAll('#filter .categories-box input.category:checked');const colorsElement=document.querySelectorAll('#filter .colors-box input.color:checked');const sizesElement=document.querySelectorAll('#filter .sizes-box input.size:checked');const minPrice=document.querySelector('#filter .min-price-box input').value||0
-    const maxPrice=document.querySelector('#filter .max-price-box input').value||50
-    const categories=categoriesElement.length?Array.from(categoriesElement,el=>el.value):[];const colors=colorsElement.length?Array.from(colorsElement,el=>el.value):[];const sizes=sizesElement.length?Array.from(sizesElement,el=>el.value):[];const filteredData=dataBase.filter(product=>{return(!categories.length||categories.includes(product.category))&&(!colors.length||checkArrayInArray(colors,product.color))&&(!sizes.length||checkArrayInArray(sizes,product.availableSizes))&&(minMaxPrice(minPrice,maxPrice,product.price))})
-    displayProducts(filteredData,!0)})
-    function checkArrayInArray(arr1,arr2){return arr1.some(x=>arr2.includes(x))}
-    function minMaxPrice(min,max,price){return(price>min&&price<max)}
-    const minInput=document.querySelector('#filter .min-price-box input');document.querySelector('#filter .min-price-box .minus').addEventListener('click',()=>{let decrece=Number(minInput.value)
-    if(decrece>0){decrece --
-    minInput.value=decrece}});document.querySelector('#filter .min-price-box .add').addEventListener('click',()=>{let increse=Number(minInput.value)
-    increse ++
-    minInput.value=increse});const maxInput=document.querySelector('#filter .max-price-box input');document.querySelector('#filter .max-price-box .minus').addEventListener('click',()=>{let decrece=Number(maxInput.value)
-    if(decrece>Number(minInput.value)+50){decrece --
-    maxInput.value=decrece}});document.querySelector('#filter .max-price-box .add').addEventListener('click',()=>{let increse=Number(maxInput.value)
-    increse ++
-    maxInput.value=increse})
+import dataBase from "./DataBase.js";
+
+
+
+document.querySelector('#filter .apply .apply-filter').addEventListener('click',()=>{
+    const categoriesElement = document.querySelectorAll('#filter .categories-box input.category:checked');
+    const colorsElement = document.querySelectorAll('#filter .colors-box input.color:checked');
+    const sizesElement = document.querySelectorAll('#filter .sizes-box input.size:checked');
+    const minPrice = document.querySelector('#filter .min-price-box input').value || 0
+    const maxPrice = document.querySelector('#filter .max-price-box input').value || 50
+    
+    const categories = categoriesElement.length ? Array.from(categoriesElement, el => el.value) : [];
+    const colors = colorsElement.length ? Array.from(colorsElement, el => el.value) : [];
+    const sizes = sizesElement.length ? Array.from(sizesElement, el => el.value) : [];
+
+    const filteredData = dataBase.filter(product =>{
+        return (!categories.length || categories.includes(product.category))&&
+            (!colors.length || checkArrayInArray(colors,product.color))&&
+            (!sizes.length || checkArrayInArray(sizes,product.availableSizes))&&
+            (minMaxPrice(minPrice,maxPrice,product.price));
+    })
+    displayProducts(filteredData,true)
+    
+})
+
+function checkArrayInArray(arr1,arr2){
+    return arr1.some(x => arr2.includes(x));
+}
+
+function minMaxPrice(min,max,price){
+    return (price > min && price < max)
+}
+
+
+
+
+const minInput = document.querySelector('#filter .min-price-box input');
+document.querySelector('#filter .min-price-box .minus').addEventListener('click',()=>{
+    let decrece = Number(minInput.value)
+    if(decrece > 0){
+        decrece --
+        minInput.value = decrece
+    }
+});
+document.querySelector('#filter .min-price-box .add').addEventListener('click',()=>{
+    let increse = Number(minInput.value)
+        increse ++
+        minInput.value = increse
+});
+
+
+
+const maxInput = document.querySelector('#filter .max-price-box input');
+document.querySelector('#filter .max-price-box .minus').addEventListener('click',()=>{
+    let decrece = Number(maxInput.value)
+    if(decrece > Number(minInput.value)+50){
+        decrece --
+        maxInput.value = decrece
+    }
+});
+document.querySelector('#filter .max-price-box .add').addEventListener('click',()=>{
+    let increse = Number(maxInput.value)
+        increse ++
+        maxInput.value = increse
+});
+
+
+
+
+
+
+
+
+
+
--- mobile.js
+++ mobile.js
@@ -1,5 +1,18 @@
 import mobCloseacc from "./main.js"
-$('.register-entry').on('click',()=>{$('#root #register-form').css('top','0')
-mobCloseacc()})
-$('#form .close-tab').on('click',()=>{$('#root #register-form').css('top','100vh');$('this').css('left','0')})
-$('.mobile-nav #filter-mob').on('click',()=>{$('.filter').toggleClass('active')})
+
+$('.register-entry').on('click',()=>{
+    $('#root #register-form').css('top','0')
+    mobCloseacc()
+})
+
+$('#form .close-tab').on('click',()=>{
+    $('#root #register-form').css('top','100vh');
+    $('this').css('left','0')
+})
+
+
+
+
+$('.mobile-nav #filter-mob').on('click',()=>{
+    $('.filter').toggleClass('active');
+})
